6b8859181b34d2342c3a2cca99164d8083f26cc4
Kashmiri Queen <149133736+Fatima-progmmer@users.noreply.github.com>
2024-03-31 22:02:21 -0700

    Delete 1.js

--- 1.js
+++
@@ -1,9 +0,0 @@
-let hello=document.querySelector('button')
-hello.addEventListener("click",my());
-
-function my()
-        {
-            alert("Don't be opened");
-            let name=prompt();
-            hello.textContent=("text",name);
-        }
